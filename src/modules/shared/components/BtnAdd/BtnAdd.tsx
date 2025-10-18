e788b4ea134a010f781d2f2356a7339e6bad32d4
Olha Lishenko <lishenko.olha@gmail.com>
2025-10-18 13:03:03 +0300

    init save

--- src/modules/shared/components/BtnAdd/BtnAdd.tsx
+++ src/modules/shared/components/BtnAdd/BtnAdd.tsx
@@ -12,19 +12,19 @@ export const BtnAdd: React.FC<BtnAddProps> = ({ productId }) => {
   const { cartList, setCartList } = useContext(CartContext);
   const { setNotification } = useContext(NotificationContext);
 
-  const addToCart = async (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-    event.stopPropagation();
+  // const addToCart = async (event: React.MouseEvent<HTMLButtonElement>) => {
+  //   event.preventDefault();
+  //   event.stopPropagation();
 
-    if (cartList.includes(productId)) {
-      const filteredCartList = [...cartList].filter(id => id !== productId);
-      setCartList(filteredCartList);
-    }
+  //   if (cartList.includes(productId)) {
+  //     const filteredCartList = [...cartList].filter(id => id !== productId);
+  //     setCartList(filteredCartList);
+  //   }
 
-    setCartList([...cartList, productId]);
-    setNotification(true);
-    setTimeout(() => setNotification(false), 2000);
-  };
+  //   setCartList([...cartList, productId]);
+  //   setNotification(true);
+  //   setTimeout(() => setNotification(false), 2000);
+  // };
 
   return (
     <button onClick={addToCart} className="btn-add">
